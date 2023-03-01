b58ea67262491449fbf5fd6e7bd2f14b8dff820b
40476 <63472353+40476@users.noreply.github.com>
2023-03-01 11:07:22 +0000

    no desc

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "PLEASE CHECK AND UPDATE YOUR 'SCONFIG.JSON' IT MIGHT CONTAIN INCORRECT SETTINGS"
+bash npm install flatted
+bash npm install express
+bash npm install socket.io
 bash node index.js
